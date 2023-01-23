13b67419b1fb6452d41d44db7f3ffb7a99b51777
hrudai2002 <hrudai2002@gmail.com>
2023-01-23 22:15:31 +0530

    random matrix

---
+++ examples/random_matrix.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "../random_generator_lib.h" 
+
+
+int main() {
+    std::vector<std::vector<int>> mat = random_matrix(3, 3, 100, 200);
+
+    int row = mat.size(), col = mat[0].size();
+
+    for(int i = 0; i < row; ++i) {
+        for(int j = 0; j < col; ++j) {
+            std::cout << mat[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+}
