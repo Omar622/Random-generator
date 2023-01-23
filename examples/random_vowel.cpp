9282ac088cd633a01770f519a6304a19ee957c48
hrudai2002 <hrudai2002@gmail.com>
2023-01-23 15:43:51 +0530

    random vowel generator

---
+++ examples/random_vowel.cpp
@@ -0,0 +1,10 @@
+#include <iostream> 
+
+#include "../random_generator_lib.h" 
+
+int main() {
+
+    std:: cout << random_vowel_lower() << "\n";
+
+    std:: cout << random_vowel_upper() << "\n";
+}
